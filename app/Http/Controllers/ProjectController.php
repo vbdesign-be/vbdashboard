c568177cb687f3b4bde867c1c2c670bd5663ee7c
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-03-07 09:23:16 +0100

    foreachloop projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,19 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Clickup;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 
 class ProjectController extends Controller
 {
-    public function projects(Request $request){
+    public function projects(Request $request)
+    {
         $user = Auth::user();
-        if(!$user->didLogin){
+        if (!$user->didLogin) {
             $request->session()->flash('notification', 'Welkom op je dashboard, hieronder kan je je gegevens controleren en veranderen.');
             return redirect('/profiel');
+            
         }
 
-
         $data['user'] = $user;
-        return view('projects/projects', $data);
+
+        $clickup = Clickup::find(1);
+        $token = $clickup->token;
         
+        $url = 'https://app.clickup.com/api/v2/list/40397755/task';
+
+        $response = Http::withToken($token)->get($url);
+
+        $data['projects'] = json_decode($response->body())->tasks;
+
+        //projecten filteren op company_id of teamleader_id
+        
+        return view('projects/projects', $data);
     }
 }
