e15fb914ef7167c6988f73d6b6ae2379063ca229
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-03-07 11:49:29 +0100

    getting projects from teamleader for company

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Clickup;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
+use MadeITBelgium\TeamLeader\Facade\TeamLeader;
+use App\Models\User;
 
 class ProjectController extends Controller
 {
@@ -16,11 +18,38 @@ class ProjectController extends Controller
         if (!$user->didLogin) {
             $request->session()->flash('notification', 'Welkom op je dashboard, hieronder kan je je gegevens controleren en veranderen.');
             return redirect('/profiel');
-            
         }
 
-        $data['user'] = $user;
+        //projecten uit teamleader halen voor een bepaald bedrijf
+        teamleaderController::reAuthTL();
 
+        $resp = TeamLeader::crm()->contact()->info($user->teamleader_id);
+
+        $companies = $resp->data->companies;
+        
+        foreach($companies as $c){
+            $company_id = $c->company->id;
+            $comps[] = TeamLeader::crm()->company()->info($company_id);
+        }
+
+        //voor elk bedrijf de projecten eruit filteren
+        
+
+        foreach($comps as $c){
+            $projecten = TeamLeader::crm()->company()->getProjects($c->data->id);
+        }
+
+        dd($projecten);
+
+        
+
+
+        
+        // return view('projects/projects', $data);
+    }
+
+    public function getCompanyId()
+    {
         $clickup = Clickup::find(1);
         $token = $clickup->token;
         
@@ -28,10 +57,7 @@ class ProjectController extends Controller
 
         $response = Http::withToken($token)->get($url);
 
-        $data['projects'] = json_decode($response->body())->tasks;
-
-        //projecten filteren op company_id of teamleader_id
-        
-        return view('projects/projects', $data);
+        $tasks = json_decode($response->body())->tasks;
+        $data['projects'] = $tasks;
     }
 }
