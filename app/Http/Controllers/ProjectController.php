094d99372f40abb64373be92f460300dcb63b564
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-03-08 20:49:54 +0100

    better uplaod files

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -118,14 +118,22 @@ class ProjectController extends Controller
         //project ophalen
         $project = TeamLeader::crm()->company()->getProjectDetail($id)->data;
         
-        //juiste map van de drive halen
+        // juiste map van de drive halen
         $folder = $project->title;
         $contents = collect(Storage::disk("google")->listContents('/', false));
         $dir = $contents->where('type', '=', 'dir')->where('filename', '=', $folder)->first(); 
 
-        //fotos opslagen in google
-        for($x = 0; $x < count($request->fotos); $x++){
-            Storage::disk("google")->putFileAs($dir['path'], $request->fotos[$x], $project->title." ".$x.".jpg");
+        // fotos opslagen in google
+        
+            //naam van project plus nummer
+        // for($x = 0; $x < count($request->fotos); $x++){
+        //     Storage::disk("google")->putFileAs($dir['path'], $request->fotos[$x], $project->title." ".$x.".jpg");
+        // }
+        
+            //naam van de file op de pc van de klant
+        foreach($request->file('fotos') as $foto){
+            // dd($foto->getClientOriginalName());
+            Storage::disk("google")->putFileAs($dir['path'], $foto, $foto->getClientOriginalName());
         }
 
         
