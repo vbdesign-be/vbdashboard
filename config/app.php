11365033e5ac4116cc4b898b0effe1dc168b1c99
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-04-08 17:05:52 +0200

    test

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        MadeITBelgium\TeamLeader\ServiceProvider\TeamLeader::class,
         App\Providers\GoogleDriveServiceProvider::class,
         
 
@@ -231,7 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        
+        'TeamLeader' => MadeITBelgium\TeamLeader\Facade\TeamLeader::class,
         
 
     ],
