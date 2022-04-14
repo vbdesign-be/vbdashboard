2b9c2759aa16cb4de671cfc03e4f679d1d3e7919
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-04-14 09:46:32 +0200

    working with testemail

--- app/Jobs/checkDnsINfo.php
+++ app/Jobs/checkDnsINfo.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use App\Http\Controllers\cloudflareController;
 use App\Http\Controllers\QboxController;
+use App\Models\Emailtest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -44,17 +45,38 @@ class checkDnsINfo implements ShouldQueue
      */
     public function handle()
     {
+        $test = new Emailtest();
+        $test->test = "1";
+        $test->save();
         sleep(20);
         QboxController::checkDns(strtolower($this->resource_code));
+        $test->test = "2";
+        $test->save();
         sleep(40);
         $record = QboxController::getDKIM(strtolower($this->resource_code));
+        $test->test = "3";
+        $test->save();
         cloudflareController::createMXRecord($this->check[0]->id, 1);
+        $test->test = "4";
+        $test->save();
         cloudflareController::createMXRecord($this->check[0]->id, 2);
+        $test->test = "5";
+        $test->save();
         cloudflareController::createSPFRecord($this->check[0]->id);
+        $test->test = "6";
+        $test->save();
         cloudflareController::createDKIMRecord($this->check[0]->id, $record);
+        $test->test = "7";
+        $test->save();
         cloudflareController::createDMARCRecord($this->check[0]->id);
+        $test->test = "8";
+        $test->save();
         $newEmail = QboxController::makeEmail($this->front, $this->resource_code, $this->password, $this->user->data->first_name);
+        $test->test = "9";
+        $test->save();
         QboxController::verifyMX(strtolower($this->resource_code));
+        $test->test = "10";
+        $test->save();
         $this->emailOrder->status = "active";
         $this->emailOrder->resource_code = $newEmail->resource_code;
         $this->emailOrder->save();          
