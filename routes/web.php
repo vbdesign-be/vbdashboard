4fb8a7596ffafa5a8edaa15bc5570073986dead6
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-04-19 14:54:17 +0200

    domeincontroller

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\teamleaderController;
 use App\Http\Controllers\ClickupController;
 use App\Http\Controllers\FreshdeskController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\DomeinController;
 use App\Mail\UserLoginMail;
 use App\Models\Order;
 use App\Models\Vimexx;
