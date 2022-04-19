6dbe5a4de6e33952cf18d18afd1fecd5b1b988e6
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-04-19 14:10:06 +0200

    working with notitie

--- database/migrations/2022_04_19_140612_create_notities_table.php
+++ database/migrations/2022_04_19_140612_create_notities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotesTable extends Migration
+class CreateNotitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateNotesTable extends Migration
      */
     public function up()
     {
-        Schema::create('notes', function (Blueprint $table) {
+        Schema::create('notities', function (Blueprint $table) {
             $table->id();
             $table->foreignId('ticket_id');
             $table->longText('text');
@@ -28,6 +28,6 @@ class CreateNotesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notes');
+        Schema::dropIfExists('notities');
     }
 }
