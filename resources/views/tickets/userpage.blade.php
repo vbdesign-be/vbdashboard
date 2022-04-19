c9e0a08606aa26b7dd380722b5b2c2145528509f
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-04-19 11:44:21 +0200

    timeline

--- resources/views/tickets/userpage.blade.php
+++ resources/views/tickets/userpage.blade.php
@@ -73,9 +73,32 @@
             @forelse($timeline as $time)
             <div class="ticket p-4 mb-4 bg-white shadow rounded w-full mx-auto">
                     @if(isset($time['ticket_id']))
-                    reactie
+                    <div class="ticket p-4 mb-4 bg-white shadow rounded w-full mx-auto">
+                    <div class="ticket__info">
+                        <p>Reactie</p>
+                        <p>Nr: {{$time['ticket_id']}}  <a href="/ticket/{{$time['ticket_id']}}"><strong>{{substr($time['text'],0,25)}}...</strong></a></p>
+                    </div>
+                </div>
                     @else
-                    ticket
+                    <div class="ticket p-4 mb-4 bg-white shadow rounded w-full mx-auto">
+                    <div class="ticket__info">
+                      <p>Ticket</p>
+                        
+                        <p>Nr: {{$time['id']}}  <a href="/ticket/{{$time['id']}}"><strong>{{$time['subject']}}</strong></a></p>
+                        
+                          <a href="/ticket/account/{{$time['user_id']}}">{{$ticket->user->firstname}} {{$ticket->user->lastname}}</a>
+                        
+                    </div>
+                    <div class="ticket_tags">
+                        <p>Tag:</p>
+                        <p>{{$time['tag']}}</p>
+                    </div>
+                    <div class="ticket_filters self-end">
+                        <p>{{$time['priority']}}</p>
+                        <p>{{$time['status']}}</p>
+                        <p>{{$ticket['type']}}</p>
+                    </div>
+                </div>
                     @endif
                     
                     
