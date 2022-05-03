f150e27c3fd7e0e33f19082cee8ff80633f5622c
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-03 09:54:53 +0200

    code cleanuop molliecontroller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -61,7 +61,6 @@ class LoginController extends Controller
         }
     }
 
-
     //user logout
     public function logout(){
         Auth::logout();
--- app/Http/Controllers/MollieController.php
+++ app/Http/Controllers/MollieController.php
@@ -9,12 +9,7 @@ use Illuminate\Support\Facades\Http;
 
 class MollieController extends Controller
 {
-    public static  function getPayments(){
-        $token = env('MOLLIE_TOKEN');
-        $response = Http::withToken($token)->get('https://api.mollie.com/v2/payments');
-        return json_decode($response)->_embedded;
-    }
-
+    //wanneer een gebruiker een domeinnaam wilt kopen
     public static function createPayment($price, $domeinnaam){
         $order = Order::where('domain', $domeinnaam)->first();
         $token = env('MOLLIE_TOKEN');
@@ -32,14 +27,15 @@ class MollieController extends Controller
           ]
         ];
 
-        
-        $res = Http::withToken("test_g6CCJx8E7JFpwCM2j77wNW8M8zQ8NC")->post('https://api.mollie.com/v2/payments', $data);
+        $res = Http::withToken($token)->post('https://api.mollie.com/v2/payments', $data);
         $ans = json_decode($res->body());
+        //redirecten naar de redirect link van mollie. Hier word de betaling uitgevoerd
         header("Location: {$ans->_links->checkout->href}");
         exit;
         
     }
 
+    //wanneer een gebruiker een email wilt kopen
     public static function createPaymentEmail($price, $email, $front, $password){
         $emailOrder = EmailOrder::where('email', $email)->first();
         $token = env('MOLLIE_TOKEN');
@@ -57,13 +53,14 @@ class MollieController extends Controller
           ]
         ];
 
-        $res = Http::withToken("test_g6CCJx8E7JFpwCM2j77wNW8M8zQ8NC")->post('https://api.mollie.com/v2/payments', $data);
+        $res = Http::withToken($token)->post('https://api.mollie.com/v2/payments', $data);
         $ans = json_decode($res->body());
         header("Location: {$ans->_links->checkout->href}");
         exit;
 
     }
 
+    //wanneer een gebruiker een site wilt transferen
     public static function createPaymentTransfer($price, $domain, $code){
         $order = Order::where('domain', $domain)->first();
         $token = env('MOLLIE_TOKEN');
@@ -80,12 +77,13 @@ class MollieController extends Controller
                 'order_id' => $order->id,
           ]
         ];
-        $res = Http::withToken("test_g6CCJx8E7JFpwCM2j77wNW8M8zQ8NC")->post('https://api.mollie.com/v2/payments', $data);
+        $res = Http::withToken($token)->post('https://api.mollie.com/v2/payments', $data);
         $ans = json_decode($res->body());
         header("Location: {$ans->_links->checkout->href}");
         exit;
     }
 
+    //wanneer een gebruiker een factuur wilt betalen
     public static function createPaymentFactuur($factuur_id, $price, $number){
         $token = env('MOLLIE_TOKEN');
         $url = env('MOLLIE_URL');
@@ -102,7 +100,8 @@ class MollieController extends Controller
                 'factuur_nummer' => $number,
           ]
         ];
-        $res = Http::withToken("test_g6CCJx8E7JFpwCM2j77wNW8M8zQ8NC")->post('https://api.mollie.com/v2/payments', $data);
+
+        $res = Http::withToken($token)->post('https://api.mollie.com/v2/payments', $data);
         $ans = json_decode($res->body());
         header("Location: {$ans->_links->checkout->href}");
         exit;
