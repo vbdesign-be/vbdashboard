a3b08cce5db6dcdb48c0725cb91dd7fa2380d7c6
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-03 09:48:34 +0200

    logincontroller code cleanup

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -22,82 +22,36 @@ class LoginController extends Controller
         return view('login', $data);
     }
 
-
-    //saving a new account in database
-    public function store(Request $request){
-
-        
-
-        //checking
-        $credentials = $request->validate([
-            'voornaam' => 'required|max:255',
-            'familienaam' => 'required|max:255',
-            'email' => 'required|email',
-            'bedrijfsnaam' => 'required|max:255',
-            'bedrijfsemail' => 'required|email',
-            'btw-nummer' => 'required|max:255'
-        ]);
-        
-        //saving a new user
-        $user = new User();
-        $user->firstname = $request->input('voornaam');
-        $user->lastname = $request->input('familienaam');
-        $user->email = $request->input('email');
-        $user->gsm = $request->input('gsm');
-        $user->save();
-
-        $newUser = User::where('email', $request->input('email'))->first();
-
-        $company = new Company();
-        $company->name = $request->input('bedrijfsnaam');
-        $company->email = $request->input('bedrijfsemail');
-        $company->VAT = $request->input('btw-nummer');
-        $company->phone = $request->input('telefoon');
-        $company->adress = $request->input('straat');
-        $company->postalcode = $request->input('postcode');
-        $company->city = $request->input('plaats');
-        $company->sector = $request->input('sector');
-        $company->user_id = $newUser->id;
-        $company->save();
-
-
-        $request->flash();
-
-        $request->session()->flash('message', 'je account is aangemaakt, log in om ons platform te gebruiken');
-
-        //todo: saving data of user in teamleader via api
-
-        return redirect('/login');
-    }
-
-    //function wich checks of user canlogin
+    //functie die kijkt of een gebruiker kan inloggen
     public function canLogin(Request $request){
-        //checking of email is filled in
+        //checken of het emailveld is ingevuld
         $credentials = $request->validate([
             'email' => 'required|email'
         ]);
 
-        //getting data of the user out of database
+        //de user gaan halen uit de database
         $user = User::where('email', $request->input('email'))->first();
 
         
         if(!$user){
-            //no user found:
+            //geen gebruiker gevonden:
             $request->session()->flash('error', 'Er is geen account gevonden met dit emailadres');
             return redirect('login');
         }else{
-            //user found:
+            //gebruiker gevonden:
             $generator = new LoginUrl($user);
             
-            //generating the loginlink for in the mail
+            //genereeren van de magic ogin link voor in de mail
             $data['url'] = $generator->generate();
+
+            //alle info over de gebruiker gaan halen
             teamleaderController::reAuthTL();
             $data['user'] = Teamleader::crm()->contact()->info($user->teamleader_id)->data;
 
-            //sending the email
+            //verzenden van de loginmail
             Mail::to($user->email)->send(new UserLoginMail($data));
 
-            //message flashen
+            //gebruiker feedback geven
             $request->flash();
             $request->session()->flash('message', 'We hebben een mail gestuurd naar ' . $user->email . ' met een loginlink');
             
