b3f8032da420d1530b8069e0eb530b161fe353a9
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-03 09:45:11 +0200

    code cleanup facturen

--- app/Http/Controllers/FacturenController.php
+++ app/Http/Controllers/FacturenController.php
@@ -10,6 +10,7 @@ use Vbdesign\Teamleader\Facade\Teamleader;
 
 class FacturenController extends Controller
 {
+    //lijst met alle facturen
     public function getFacturen(){
         teamleaderController::reAuthTL();
         //wie ingelogd?
@@ -22,6 +23,7 @@ class FacturenController extends Controller
         }
         $data['comps'] = $comps;
         
+        //Voor elk bedrijf dat de klant heeft de facturen gaan ophalen
         foreach ($comps as $c) {
             //kunnen maar dan 20 zijn dus enkele instantie in de array moet een offerte zijn
             $facturen[] = Teamleader::deals()->getInvoices(['filter'=> ['customer' => ['type' => 'company', 'id' => $c->data->id] ], 'page' => ['number' => 1, 'size' => 100]])->data;
@@ -34,6 +36,7 @@ class FacturenController extends Controller
         return view('facturen/facturen', $data);
     }
 
+    //downloaden van een factuur
     public function downloadFactuur($factuur_id){
         teamleaderController::reAuthTL();
         $teamleader_id = Auth::user()->teamleader_id;
@@ -49,7 +52,8 @@ class FacturenController extends Controller
             $facturen[] = Teamleader::deals()->getInvoices(['filter'=> ['customer' => ['type' => 'company', 'id' => $c->data->id] ], 'page' => ['number' => 1, 'size' => 100]])->data;
         }
 
-        //security
+
+        //security zodat enkel de gebruiker de factuur kan downloaden.
         for($x = 0; $x < count($facturen); $x++){
             foreach($facturen[$x] as $fac){
                 if($fac->id === $factuur_id){
@@ -57,7 +61,8 @@ class FacturenController extends Controller
                 }
             }
         }
-
+        
+        //als download niet leeg is naar de download link anders abort.
         if(!empty($download)){
             return redirect($download->data->location);
         }else{
@@ -65,13 +70,13 @@ class FacturenController extends Controller
         }
     }
 
+    //functie waar de gebruiker een factuur betaald
     public function betaalFactuur($factuur_id){
         teamleaderController::reAuthTL();
         //factuur info
         $factuur = Teamleader::deals()->getInvoice(['id' => $factuur_id]);
 
         //security
-        $bedrijf = $factuur->invoicee->name;
         $teamleader_id = Auth::user()->teamleader_id;
         $user = Teamleader::crm()->contact()->info($teamleader_id)->data;
         $companies = $user->companies;
@@ -79,24 +84,36 @@ class FacturenController extends Controller
             $company_id = $c->company->id;
             $comps[] = Teamleader::crm()->company()->info($company_id);
         }
-        foreach($comps as $c){
-            if($c->data->name === $bedrijf){
-                $check = true;
+        foreach ($comps as $c) {
+            $facturen[] = Teamleader::deals()->getInvoices(['filter'=> ['customer' => ['type' => 'company', 'id' => $c->data->id] ], 'page' => ['number' => 1, 'size' => 100]])->data;
+        }
+
+
+        //security zodat enkel de gebruiker de factuur kan downloaden.
+        for($x = 0; $x < count($facturen); $x++){
+            foreach($facturen[$x] as $fac){
+                if($fac->id === $factuur_id){
+                    $check = true;
+                }
             }
         }
+        
+        //als check niet bestaat is de factuur niet van de persoon dus abort
         if(!isset($check)){
             abort(403);
         }
 
         //prijs uit de factuur halen
         $price = strval($factuur->total->payable->amount);
-            // als er maar 1 cijfer achter punt->nul bijzetten(anders werkt mollie niet)
-            $priceExplode = explode(".", $price);
-            if(!isset($priceExplode[1])){
-                $price = $price.".00";
-            }elseif(strlen($priceExplode[1]) === 1){
-                $price = $price."0";
-            }
+
+        // als er maar 1 cijfer achter punt->nul bijzetten(anders werkt mollie niet)
+        $priceExplode = explode(".", $price);
+        if(!isset($priceExplode[1])){
+            $price = $price.".00";
+        }elseif(strlen($priceExplode[1]) === 1){
+            $price = $price."0";
+        }
+
         //invoice number uit de factuur halen
         $number = $factuur->invoice_number;
         $explodeNumber = explode(" ", $number);
@@ -105,6 +122,7 @@ class FacturenController extends Controller
         MollieController::createPaymentFactuur($factuur_id, $price, $realNumber);
     }
 
+    //mollie payment is volbracht
     public function payedFactuur(Request $request){
         $credentials = $request->validate([
             'factuur_id' => 'required',
@@ -127,10 +145,12 @@ class FacturenController extends Controller
         teamleaderController::reAuthTL();
         $res = Teamleader::deals()->invoicePayed(['id' => $factuur_id, 'payment' => ['amount' => $price, 'currency' => 'EUR'], 'paid_at' => $realDate]);
 
+        //gebruiker laten weten dat de betaling gelukt is en redirecten
         $request->session()->flash('message', 'Factuur: '.$realNumber.' is betaald.');
         return redirect('facturen');
     }
 
+    //pagina met lijst met alle creditnotas
     public function getCreditnotas(){
         teamleaderController::reAuthTL();
         //wie ingelogd?
@@ -148,23 +168,18 @@ class FacturenController extends Controller
             $facturen[] = Teamleader::deals()->getInvoices(['filter'=> ['customer' => ['type' => 'company', 'id' => $c->data->id] ], 'page' => ['number' => 1, 'size' => 100]])->data;
         }
 
-    
-        //alle notas gaan halen die bij de facturen passen
-        // foreach($facturen[0] as $f){
-        //     $notes['notes'][] = Teamleader::deals()->getCreditnotes(['filter' => [ 'invoice_id' => $f->id]]);
-        //     $notes['notes']['title'][] = Teamleader::deals()->getInvoice(['id' => $f->id]);
-        // };
+        //voor elke factuur de creditnota en titel van de factuur gaan halen.
         for($x = 0; $x < count($facturen[0]); $x++){
             $notes[$x]['note'] = Teamleader::deals()->getCreditnotes(['filter' => [ 'invoice_id' => $facturen[0][$x]->id]]);
             $notes[$x]['title'] = $facturen[0][$x]->invoicee->name;
         }
 
-        //dd($notes[1]);
-        
+        //gebruiker redirecten
         $data['creditnotas'] = $notes;
         return view('facturen/creditnotas', $data);
     }
 
+    //het downloaden van een creditnota
     public function downloadCreditnota($credit_id){
         teamleaderController::reAuthTL();
         $teamleader_id = Auth::user()->teamleader_id;
@@ -178,10 +193,12 @@ class FacturenController extends Controller
         //security
         $credit = Teamleader::deals()->getOneCreditnote(['id' => $credit_id]);
         foreach($comps as $c){
-            if($c->data->name === $credit->invoicee->name){
+            if($c->data->id === $credit->invoicee->customer->id){
                 $check = true;
             }
         }
+        
+        //als check bestaat is de creditnota van de ingelogde persoon
         if(isset($check)){
             $download = Teamleader::deals()->downloadCreditnota(['id' => $credit_id, 'format' => 'pdf']);
             return redirect($download->data->location);
