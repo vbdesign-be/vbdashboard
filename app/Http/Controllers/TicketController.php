c16351861431b2b72ad75cfbb81d963479df8481
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-03 12:12:29 +0200

    ticket controller code cleanup

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -24,11 +24,14 @@ use Illuminate\Support\Facades\Mail;
 
 class TicketController extends Controller
 {
-    public function getTickets(Request $request)
-    {
+    //pagina met een lijst van alle tickets voor een specifieke agent(momenteel is er maar 1 agent);
+    public function getTickets(Request $request){
+        //checken of de ingelogde gebruiker een agent is
         if (Auth::user()->isAgent !== 1) {
             abort(403);
         }
+
+        //kijken wat de input van de filter is en deze toepassen
         $filterInput = $request->input('filter');
         switch($filterInput){
             case("priority"):
@@ -51,20 +54,22 @@ class TicketController extends Controller
 
         $data['tickets'] = Ticket::where('agent_id', Auth::id())->orderBy($filter, $direction)->get();
         $data['filter'] = $filterInput;
-        //dd($data['tickets']);
         return view('tickets/tickets', $data);
     }
 
-    public function detailTicket($ticket_id)
-    {
+    //detailpagina van een ticket
+    public function detailTicket($ticket_id){
+        //checken of de ingelogde gebruiker een agent is
         if (Auth::user()->isAgent !== 1) {
             abort(403);
         }
 
+        //checken of het ticket bij de agent hoort
         $data['ticket'] = Ticket::find($ticket_id);
         if ($data['ticket']->agent_id !== Auth::id()) {
             abort(403);
         }
+
         $data['ticket']->isOpen = 1;
         $data['ticket']->save();
         $data['reactions'] = Reaction::where('ticket_id', $ticket_id)->orderBy('created_at')->get();
@@ -74,33 +79,41 @@ class TicketController extends Controller
         return view('tickets/ticketDetail', $data);
     }
 
-    public function getUser($user_id)
-    {
+    //detailpagina over de tickets van een bepaalde user
+    public function getUser($user_id){
+        //checken of de ingelogde gebruiker een agent is
         if (Auth::user()->isAgent !== 1) {
             abort(403);
         }
 
         $data['user'] = User::find($user_id);
+
+        //alle tickets van een user verzamelen
         $data['tickets'] = Ticket::where('user_id', $user_id)->orderBy('id', 'desc')->get();
+
+        //alle reacties van een gebruiker verzamelen
         foreach($data['tickets'] as $ticket){
             $allReactions[] = Reaction::where('ticket_id', $ticket->id)->get();
         }
         $data['reactions'] = $allReactions[0];
 
+        //alle tickets + reactions verzamelen en filteren op datum voor de tijdlijn
         $tickets = $data['tickets']->toArray();
         $reactions = $data['reactions']->toArray();
         $everything = array_merge($tickets, $reactions);
         $timeLine = collect($everything)->sortByDesc('created_at')->all();
-        //dd($timeLine);
         $data['timeline'] = $timeLine;
         return view('tickets/userpage', $data);
     }
 
+    //status van een ticket updaten
     public function statusUpdate(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if (Auth::user()->isAgent !== 1) {
             abort(403);
         }
 
+        //checken of alle informatie is meegegeven
         $credentials = $request->validate([
             'status' => 'required',
             'ticket_id' => 'required',
@@ -108,27 +121,32 @@ class TicketController extends Controller
 
         $status = $request->input('status');
         $ticket_id = $request->input('ticket_id');
-         //security
+
+         //security, checken of het ticket wel bij de agent hoort
          $ticket = Ticket::find($ticket_id);
          if($ticket->agent_id !== Auth::id()){
              abort(403);
          }
+
         //update status
         $ticket->status_id = $status;
         $ticket->save();
 
-        //commentaar
-        $request->session()->flash('message', 'Ticket is geupdate');
+        //agent laten weten dat het ticket is geupdate
+        $request->session()->flash('message', 'Status van het ticket is geupdate');
         
         //redirecten
         return redirect('/ticket/'.$ticket_id);
     }
 
+    //prioriteit van een ticket wijzigen
     public function priorityUpdate(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if (Auth::user()->isAgent !== 1) {
             abort(403);
         }
 
+        //checken of alle informatie is meegegeven
         $credentials = $request->validate([
             'priority' => 'required',
             'ticket_id' => 'required',
@@ -136,27 +154,32 @@ class TicketController extends Controller
 
         $priority = $request->input('priority');
         $ticket_id = $request->input('ticket_id');
-         //security
-         $ticket = Ticket::find($ticket_id);
-         if($ticket->agent_id !== Auth::id()){
-             abort(403);
-         }
-        //update status
+        
+        //security, checken of het ticket wel bij de agent hoort
+        $ticket = Ticket::find($ticket_id);
+        if($ticket->agent_id !== Auth::id()){
+            abort(403);
+        }
+
+        //update prioriteit
         $ticket->priority_id = $priority;
         $ticket->save();
 
-        //commentaar
+        //agent laten weten dat het ticket is geupdate
         $request->session()->flash('message', 'Ticket is geupdate');
         
         //redirecten
         return redirect('/ticket/'.$ticket_id);
     }
 
+    //statuc van een ticket updaten
     public function typeUpdate(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if (Auth::user()->isAgent !== 1) {
             abort(403);
         }
 
+        //checken of alle informatie is meegegeven
         $credentials = $request->validate([
             'type' => 'required',
             'ticket_id' => 'required',
@@ -164,27 +187,31 @@ class TicketController extends Controller
 
         $type = $request->input('type');
         $ticket_id = $request->input('ticket_id');
-         //security
-         $ticket = Ticket::find($ticket_id);
-         if($ticket->agent_id !== Auth::id()){
-             abort(403);
-         }
-        //update status
+
+        //security, checken of het ticket wel bij de agent hoort
+        $ticket = Ticket::find($ticket_id);
+        if($ticket->agent_id !== Auth::id()){
+            abort(403);
+        }
+
+        //update type
         $ticket->type_id = $type;
         $ticket->save();
 
-        //commentaar
+        //agent laten weten dat het ticket is geupdate
         $request->session()->flash('message', 'Ticket is geupdate');
         
         //redirecten
         return redirect('/ticket/'.$ticket_id);
     }
 
+    //reactie van de agent toevoegen
     public function addReactionAgent(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if (Auth::user()->isAgent !== 1) {
             abort(403);
         }
-        //checking credentials
+        //checken of alle velden zijn ingevuld
         $credentials = $request->validate([
             'reactie' => 'required',
         ]);
@@ -192,14 +219,12 @@ class TicketController extends Controller
         $body = $request->input('reactie');
         $ticket_id = $request->input('ticket_id');
 
-        
-        //security
+        //security, checken of het ticket wel bij de agent hoort
         $ticket = Ticket::find($ticket_id);
         if($ticket->agent_id !== Auth::id()){
             abort(403);
         }
 
-        
         //reactie opslaan
         $reaction = new Reaction();
         $reaction->ticket_id = $ticket_id;
@@ -207,7 +232,7 @@ class TicketController extends Controller
         $reaction->text = $body;
         $reaction->save();
 
-        //attachments 
+        //kijken of er attachments zijn en deze oplaand
         if (!empty($request->file('attachments'))) {
             foreach ($request->file('attachments') as $attachment) {
                 $imageSrc = time().'.'.$attachment->extension();
@@ -229,7 +254,6 @@ class TicketController extends Controller
         $data['attachments'] = AttachmentReaction::where('reaction_id', $reaction->id)->get();
         $data['cc'] = Cc::where('ticket_id', $ticket_id)->get();
         
-        
         if(!empty($ticket->user_id)){
             Mail::to($ticket->user->email)->send(new TicketReactionMail($data));
         }else{
@@ -242,17 +266,20 @@ class TicketController extends Controller
 
     }
 
-
+    //emailadress toevoegen aan spam
     public function spam(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if(Auth::user()->isAgent !== 1){
             abort(403);
         }
 
+        //checken of alle velden zijn ingevuld
         $credentials = $request->validate([
             'email' => 'required',
             'ticket_id' => 'required',
         ]);
 
+        //emailadress in spam table opslaan
         $email = $request->input('email');
         $ticket_id = $request->input('ticket_id');
         $spam = new Spam();
@@ -263,7 +290,9 @@ class TicketController extends Controller
         return redirect('/ticket/'.$ticket_id);
     }
 
+    //ticket samenvoegenpagina
     public function samenvoegPage($ticket_id){
+        //checken of de ingelogde gebruiker wel een agent is
         if(Auth::user()->isAgent !== 1){
             abort(403);
         }
@@ -275,18 +304,19 @@ class TicketController extends Controller
             $allTickets = Ticket::where('email', $ticket->email)->where('id', '!=', $ticket_id)->get();
         }
         
-
         $data['ticket'] = $ticket;
         $data['allTickets'] = $allTickets;
         return view('tickets/ticketsamenvoeg', $data);
-
     }
 
+    //2 tickets samenvoegen
     public function ticketsMerge(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if(Auth::user()->isAgent !== 1){
             abort(403);
         }
 
+        //checken of er 2 tickets zijn meegestuurd
         $credentials = $request->validate([
             'ticket1' => 'required',
             'ticket2' => 'required',
@@ -297,7 +327,7 @@ class TicketController extends Controller
             return redirect('/ticket/samenvoegen/'.$request->input('ticket1'));
         }
 
-
+        //de 2 tickets gaan zoeken in de database
         $ticket1 = Ticket::find($request->input('ticket1'));
         $ticket2 = Ticket::find($request->input('ticket2'));
 
@@ -313,9 +343,9 @@ class TicketController extends Controller
         return redirect('/ticket/'.$merged);
     }
 
+    //fucntie die 2 ticketten samenvoegd
     private function mergeTickets($old, $new){
         //van het nieuw ticket een reactie maken
-        //dd($new);
         $newReaction = new Reaction();
         $newReaction->ticket_id = $old->id;
         if(!empty($new->user_id)){
@@ -328,7 +358,6 @@ class TicketController extends Controller
         $newReaction->save();
 
         //alle attachments van dat ticket tot attachment reactie maken
-        
         $allAttachments = $new->attachmentsTicket;
         foreach($allAttachments as $att){
             $newAtt = new AttachmentReaction();
@@ -368,7 +397,7 @@ class TicketController extends Controller
             $reaction->save();
         }
 
-        // //notitie samenvoegen
+        //notitie samenvoegen
         $notitie = $new->notitie;
         $oldNotitie = $old->notitie;
         if(!empty($oldNotitie) && !empty($notitie)){
@@ -384,23 +413,24 @@ class TicketController extends Controller
             $notitie->delete();
         }
         
-        
-
         //new ticket verwijderen
         $new->delete();
-        
         return $old->id;
     }
 
+    //het verwijderen van een ticket
     public function deleteTicket(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if(Auth::user()->isAgent !== 1){
             abort(403);
         }
 
+        //checken of alle velden zijn ingevuld
         $credentials = $request->validate([
             'ticket_id' => 'required',
         ]);
 
+        //ticket zoeken en verwijderen
         $ticket = Ticket::find($request->input('ticket_id'));
         $ticket->delete();
 
@@ -408,11 +438,14 @@ class TicketController extends Controller
         return redirect('/tickets');
     }
 
+    //ticket doorsturen naar iemand
     public function ticketSend(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if(Auth::user()->isAgent !== 1){
             abort(403);
         }
 
+        //checken of alle velden zijn ingevuld
         $credentials = $request->validate([
             'email' => 'required|email',
             'ticket_id' => 'required'
@@ -423,7 +456,8 @@ class TicketController extends Controller
 
         //ticket ophalen
         $ticket = Ticket::find($ticket_id);
-        //kijken of email niet gelijk is aan de sender van het ticket
+
+        //kijken of email niet gelijk is aan de originele sender van het ticket
         if(!empty($ticket->user_id) && $ticket->user->email === $email){
             $request->session()->flash('error', 'Email kan niet het originele adress zijn');
             return redirect('/ticket/'.$ticket_id);
@@ -433,10 +467,10 @@ class TicketController extends Controller
             $request->session()->flash('error', 'Email kan niet het originele adress zijn');
             return redirect('/ticket/'.$ticket_id);
         }
+
         $data['ticket'] = $ticket;
 
         //mail versturen met het originele ticket in
-       
         if(!empty($ticket->user_id)){
             $data['email'] = $ticket->user->email;
             Mail::to($email)->send(new sendTicket($data));
@@ -447,11 +481,11 @@ class TicketController extends Controller
 
         $request->session()->flash('message', 'Email succesvol doorgestuurd');
         return redirect('/ticket/'.$ticket_id);
-
-
     }
 
+    //user van het ticket wijzigen
     public function changeUser(Request $request){
+        //checken of de ingelogde gebruiker wel een agent is
         if(Auth::user()->isAgent !== 1){
             abort(403);
         }
@@ -466,7 +500,6 @@ class TicketController extends Controller
         $ticket = Ticket::find($request->input('ticket_id'));
         $checkUser = User::where('email', $email)->first();
         
-
         //checken of het ticket al reacties heeft => anders moet wijzigen onmogelijk zijn
         if(!empty($ticket->reactions[0])){
             $request->session()->flash('error', 'Kan afzender niet wijzigen omdat ticket reacties heeft');
@@ -487,6 +520,4 @@ class TicketController extends Controller
         return redirect('/ticket/'. $ticket->id);
 
     }
-
-    
 }
